3be2d698d6fbc54a5f6e959d74fc28309bd38d96
paul_light <dctr.waker@gmail.com>
2019-07-17 12:34:14 +0200

    Add to local Storage/Remove from Local Storage

--- appes6.js
+++ appes6.js
@@ -55,6 +55,53 @@ class UI{
     }
 }
 
+//Local storage class
+class Store { 
+   static getBooks(){
+         let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+   }
+
+   static displayBooks(){
+        const books = Store.getBooks();
+
+        books.forEach(function(book){
+         const ui = new UI;
+
+         //Add book to UI
+            ui.addBookToList(book)
+    });
+   }
+
+   static addBook(book){
+     const books = Store.getBooks();
+
+     books.push(book);
+
+     localStorage.setItem('books', JSON.stringify(books));
+   }
+
+   static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+   }
+}
+
+
+//DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+
 //Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
     //Get form values
@@ -76,6 +123,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
         //Add book to list
         ui.addBookToList(book);
 
+        //Add to Local Storage
+        Store.addBook(book);
+
         //Show seccess
         ui.showAlert('Book Added!', 'success');
 
@@ -92,7 +142,11 @@ document.getElementById('book-list').addEventListener('click', function(e){
     //Instantiate UI
     const ui = new UI();
     
+    //Delete book
     ui.deleteBook(e.target);
+
+    //Remove Books from Local Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     //Show aler message
     ui.showAlert('Book Removed!', 'success');
